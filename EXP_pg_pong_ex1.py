febc6faf3a8e08d3295060a451e6d31c222e05ec
Nenetti <akinori1090@gmail.com>
2019-01-09 19:29:22 +0900

    first

---
+++ EXP_pg_pong_ex1.py
@@ -0,0 +1,39 @@
+from pg_engine import *
+from Controller import *  # new import
+from EMG import *  # new import
+from engine import *
+
+
+class game:
+
+    def __init__(self):
+        self.start()
+        self.running = True
+        while self.running:
+            for event in pygame.event.get():
+                # checks if you've exited the game
+                if event.type == pygame.QUIT or (event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE):
+                    self.running = False
+
+            self.update()
+            pygame.display.update()
+            pygame.time.delay(16)  # 30 FPS
+
+        pygame.quit()
+
+    def start(self):
+
+        #self.player = Player(300, 300, 200, 200, pygame.image.load('Rh.jpg'))
+        self.player = Player(150, 300, 464, 188, pygame.image.load('invader1.jpeg'))
+        self.player.set_animation(2, 1)
+
+    def update(self):
+        # 画面初期化
+        screen.fill(pygame.color.THECOLORS['black'])
+
+        #self.player.update(screen)
+        self.player.update(screen)
+
+
+if __name__ == '__main__':
+    g = game()
---
+++ engine.py
@@ -0,0 +1,91 @@
+import pygame
+from Keyboard import *
+
+
+class Player:
+    def __init__(self, x, y, width, height, image):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.image = image
+        self.speed_x = 0
+        self.speed_y = 0
+        self.size_x = 0
+        self.size_y = 0
+        self.anim_no = 0
+        self.max_anim = 0
+
+        self.Controller = Keyboard()
+
+    def set_animation(self, split_x, split_y):
+        self.animation = True
+        self.split_x = split_x
+        self.split_y = split_y
+        self.size_x = self.width / split_x
+        self.size_y = self.height / split_y
+        self.max_anim = split_x * split_y
+
+    def update(self, screen):
+        self.calc_speed(screen)
+        if self.animation:
+            screen.blit(self.image, [self.x, self.y], [int(self.size_x*self.anim_no), 0, int(self.size_x), int(self.size_y)])
+            self.anim_no+=1
+            if self.anim_no>=self.max_anim:
+                self.anim_no=0
+        else:
+            screen.blit(self.image, [self.x, self.y])
+
+    def calc_speed(self, screen):
+        input = self.Controller.get_input()
+        speed_x = 0
+        speed_y = 0
+        if input == Controller.Input.Left:
+            speed_x = -5
+            if self.x + speed_x < 0:
+                speed_x = 0
+        elif input == Controller.Input.Right:
+            speed_x = 5
+            if self.x + speed_x > screen.get_width():
+                speed_x = 0
+        self.x += speed_x
+        self.y += speed_y
+
+
+class Enemy:
+    def __init__(self, x, y, width, height, image):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.image = image
+        self.speed_x = 0
+        self.speed_y = 0
+        self.animation = False
+        self.split_x = 0
+        self.split_y = 0
+        self.size_x = 0
+        self.size_y = 0
+        self.anim_no = 0
+        self.max_anim = 0
+
+    def set_animation(self, split_x, split_y):
+        self.animation = True
+        self.split_x = split_x
+        self.split_y = split_y
+        self.size_x = self.width/split_x
+        self.size_y = self.height/split_y
+        self.max_anim = split_x*split_y
+
+    def move(self, screen, speed_x, speed_y):
+        self.x += speed_x
+        self.y += speed_y
+
+    def update(self, screen):
+        if self.animation:
+            screen.blit(self.image, [self.x, self.y], [int(self.size_x*self.anim_no), 0, int(self.size_x), int(self.size_y)])
+            self.anim_no+=1
+            if self.anim_no>=self.max_anim:
+                self.anim_no=0
+        else:
+            screen.blit(self.image, [self.x, self.y])
